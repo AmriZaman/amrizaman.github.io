58b035f29556a97564fe5160ff2cf940f4b14f24
AmriZaman <54435089+AmriZaman@users.noreply.github.com>
2019-11-05 16:29:09 +0700

    Update jsnya.js

--- js/jsnya.js
+++ js/jsnya.js
@@ -51,3 +51,23 @@ var aktif = false;
           true;
       }
   }
+  function tampilkanPreview(gambar,idpreview){
+      var gb = gambar.files;
+      for (var i = 0; i < gb.length; i++){
+          var gbPreview = gb[i];
+          var imageType = /image.*/;
+          var preview=document.getElementById(idpreview);
+          var reader = new FileReader();
+          if (gbPreview.type.match(imageType)) {
+              preview.file = gbPreview;
+              reader.onload = (function(element) {
+                  return function(e) {
+                      element.src = e.target.result;
+                  };
+              })(preview);
+              reader.readAsDataURL(gbPreview);
+          }else{
+              alert("Type file tidak sesuai. Khusus image.");
+          }
+      }
+  }
